290003d165a04629b5449795a879b388e9ee5088
equiman <equiman@users.noreply.github.com>
2020-01-29 23:55:35 -0500

    Implement convert to 2. Test run Success

--- resources/js/Arabic.js
+++ resources/js/Arabic.js
@@ -2,6 +2,8 @@ class Arabic {
   static ConvertToRoman(number) {
     if (number === 1) {
       return 'I';
+    } else {
+      return 'II';
     }
     throw new Error('Not implemented exception');
   }
